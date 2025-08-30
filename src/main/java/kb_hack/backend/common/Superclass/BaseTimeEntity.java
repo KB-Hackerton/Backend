1578d9c8d5070c30eeda0db65a11e27db1f99613
bagjuna <juna.park124@gmail.com>
2025-08-30 16:36:09 +0900

    Feat: 기본 개발 환경 세팅
    - security
    - MyBatis
    - Flyway
    - MySQL
    - Lombok

---
+++ src/main/java/kb_hack/backend/common/Superclass/BaseTimeEntity.java
@@ -0,0 +1,11 @@
+package kb_hack.backend.common.Superclass;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+
+@Getter
+public abstract class BaseTimeEntity {
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+}
