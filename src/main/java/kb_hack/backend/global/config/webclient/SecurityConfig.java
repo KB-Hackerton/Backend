747fd4acda91d7e65e90919098cb6968edcad69d
juwon1105 <amy1105a@naver.com>
2025-09-02 01:14:27 +0900

    feat : 보안 임시 설정

---
+++ src/main/java/kb_hack/backend/global/config/webclient/SecurityConfig.java
@@ -0,0 +1,19 @@
+package kb_hack.backend.global.config.webclient;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(auth -> auth
+				.anyRequest().permitAll()   // ← 임시로 전체 허용
+			);
+		return http.build();
+	}
+}
