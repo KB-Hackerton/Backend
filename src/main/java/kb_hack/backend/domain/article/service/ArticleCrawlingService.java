e80167389bc786a7de87b17ec717090b1ac26506
bagjuna <juna.park124@gmail.com>
2025-09-02 14:45:15 +0900

    Feat: 관리자용 기사 크롤링 api 개발 - 소상공인뉴스 포털에서 3카테고리에서 실시간 크롤링 -  jsoup,selenium 이용 - 100개 저장

---
+++ src/main/java/kb_hack/backend/domain/article/service/ArticleCrawlingService.java
@@ -0,0 +1,34 @@
+package kb_hack.backend.domain.article.service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j // Slf4j 어노테이션으로 로거 자동 생성
+@Service
+@RequiredArgsConstructor
+public class ArticleCrawlingService {
+
+	private final CrawlingOne crawlingOne; // 새로운 크롤러 주입
+	private static final Set<String> ACRTICLE_SET = new HashSet<>();
+
+
+	static {
+		ACRTICLE_SET.add("https://www.sbma.kr/news_gisa/gisa_list.htm?gisa_category=01010000");
+		ACRTICLE_SET.add("https://www.sbma.kr/news_gisa/gisa_list.htm?gisa_category=01020000");
+		ACRTICLE_SET.add("https://www.sbma.kr/news_gisa/gisa_list.htm?gisa_category=01040000");
+	}
+
+
+	public void startCrawling() {
+		for (String url : ACRTICLE_SET) {
+			crawlingOne.crawlingOneArticle(url);
+		}
+	}
+
+
+}
