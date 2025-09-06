9095553a6b7cafb12f5bd1b1be3ab023e8378f9f
juwon1105 <amy1105a@naver.com>
2025-09-06 19:57:59 +0900

    fix : xml 파일 분리

--- src/main/java/kb_hack/backend/domain/sos/mapper/SosImageMapper.java
+++ src/main/java/kb_hack/backend/domain/sos/mapper/SosImageMapper.java
@@ -5,13 +5,10 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 
+import kb_hack.backend.domain.sos.entity.SosImage;
+import org.apache.ibatis.annotations.Mapper;
+
 @Mapper
 public interface SosImageMapper {
-
-	@Insert("""
-        INSERT INTO sos_image (sos_id, storage_key, is_deleted)
-        VALUES (#{sosId}, #{storageKey}, #{isDeleted})
-    """)
-	@Options(useGeneratedKeys = true, keyProperty = "sosImageId", keyColumn = "sos_image_id")
 	int insert(SosImage image);
 }
--- src/main/java/kb_hack/backend/domain/sos/mapper/SosMapper.java
+++ src/main/java/kb_hack/backend/domain/sos/mapper/SosMapper.java
@@ -5,13 +5,11 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Insert;
 
+
+import kb_hack.backend.domain.sos.entity.Sos;
+import org.apache.ibatis.annotations.Mapper;
+
 @Mapper
 public interface SosMapper {
-
-	@Insert("""
-        INSERT INTO sos (member_id, sos_title, sos_type, sos_content, expires_at, is_complete, is_deleted)
-        VALUES (#{memberId}, #{sosTitle}, #{sosType}, #{sosContent}, #{expiresAt}, #{isComplete}, #{isDeleted})
-    """)
-	@Options(useGeneratedKeys = true, keyProperty = "sosId", keyColumn = "sos_id")
 	int insert(Sos sos);
 }
