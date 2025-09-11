c7f301c32ffd2b8c58163091c2fe7d7f40269237
bagjuna <juna.park124@gmail.com>
2025-09-11 22:05:24 +0900

    Feat: 하드 삭제

--- src/main/java/kb_hack/backend/domain/sos/mapper/SosMapper.java
+++ src/main/java/kb_hack/backend/domain/sos/mapper/SosMapper.java
@@ -1,20 +1,11 @@
 package kb_hack.backend.domain.sos.mapper;
 
 import java.util.List;
-import java.util.Map;
 
-import kb_hack.backend.domain.sos.dto.SosDetailResponse;
 import kb_hack.backend.domain.sos.dto.SosDetailRow;
 import kb_hack.backend.domain.sos.dto.SosListResponse;
 import kb_hack.backend.domain.sos.entity.Sos;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Insert;
-
-
-import kb_hack.backend.domain.sos.entity.Sos;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface SosMapper {
